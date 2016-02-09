977654408e452f27508a2dabb64c9e970128b204
Eirik Høgmo <eirikhog@gmail.com>
2016-02-09 20:52:03 +0100

    WIP radio.

--- src/nrf24.c
+++ src/nrf24.c
@@ -48,25 +48,35 @@ nrf24_read_register(uint8_t addr, uint8_t *buffer, uint8_t len) {
     chip_deselect();
 }
 
+void nrf24_config_reg(uint8_t reg, uint8_t value) {
+    chip_select();
+    // TODO: W_REGISTER
+    chip_deselect();
+}
+
 void nrf24_init() {
     DDRD |= (1<<PD7);
     DDRB |= (1<<PB0);
 
-    // Setup SPI
     // Set output: MOSI and SCK
     DDRB = (1<<PB3)|(1<<PB5);
     // SPI Enable, Master mode, f_osc / 16
     SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
-    // TODO: Clock mode?
 
     uint8_t buff = 0;
     nrf24_read_register(0, &buff, 1);
 }
 
+static uint16_t packet_size;
+
 void nrf24_config(uint16_t channel, uint16_t size) {
     chip_select();
 
+    packet_size = size;
+
+
     chip_deselect();
 }
+
 
 
